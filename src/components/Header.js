59a94ea49ac5cf3ebb69b20368cb3479ed6915b4
1ce0ne <alexandertschernakow@gmail.com>
2023-06-28 22:12:51 +0300

    feature(css, components) use css grid, add some components

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://i.pinimg.com/originals/29/91/29/299129e3af223b5f73bf94ebd27fe8c7.png'></img>
+         </header>
+    )
+}
+
+export default Header
