344b08a149c82081c0d21774842f6313143f5026
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2023-07-18 22:21:48 +0300

    feature(18.07.23): Add auth method

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
+import { NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = (props) => {
   return (
     <header className={styles.header}>
-      <img src='https://e7.pngegg.com/pngimages/301/302/png-clipart-japan-torii-idea-sticker-t-shirt-japan-logo-sticker.png'></img>
+      <img src='https://e7.pngegg.com/pngimages/301/302/png-clipart-japan-torii-idea-sticker-t-shirt-japan-logo-sticker.png' />
+      <div className={styles.loginBlock}>
+        {props.isAuth ? props.login
+          : <NavLink to={'/login'}>Login</NavLink>
+        }
+      </div>
     </header>
   )
 }
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import messagesReducer from "./messagesReducer";
 import navbarReducer from "./navbarReducer";
 import profileReducer from "./profileReducer";
 import usersReducer from './usersReducer';
+import authReducer from './authReducer';
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   messagesPage: messagesReducer,
   navbarPage: navbarReducer,
-  usersPage: usersReducer
+  usersPage: usersReducer,
+  auth: authReducer
 });
 
 let store = legacy_createStore(reducers);
