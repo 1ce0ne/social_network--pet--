2a2a6bff1cc2d69ddf42a1159ea411b11dc36182
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2023-07-18 11:37:31 +0300

    feature(18.07.23): add pagination in Users page

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,11 +1,14 @@
 import { connect } from "react-redux";
 import Users from "./Users";
-import { followAC, setUsersAC, unfollowAC } from "../../redux/usersReducer";
+import { followAC, setCurrentPageAC, setUsersAC, unfollowAC, setTotalUsersCountAC } from "../../redux/usersReducer";
 
 
 let mapStateToProps = (state) => {
   return {
-    users: state.usersPage.users
+    users: state.usersPage.users,
+    pageSize: state.usersPage.pageSize,
+    totalUsersCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage 
   }
 }
 
@@ -13,7 +16,9 @@ let mapDispatchToProps = (dispatch) => {
   return {
     follow: (userId) => {dispatch(followAC(userId))},
     unfollow: (userId) => {dispatch(unfollowAC(userId))},
-    setUsers: (users) => {dispatch(setUsersAC(users))} 
+    setUsers: (users) => {dispatch(setUsersAC(users))},
+    setCurrentPage: (currentPage) => {dispatch(setCurrentPageAC(currentPage))},
+    setTotalUsersCount: (totalCount) => {dispatch(setTotalUsersCountAC(totalCount))}
   }
 }
 
