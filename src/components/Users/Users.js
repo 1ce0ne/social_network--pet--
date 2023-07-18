8119eda7e536f8642c4bba00287bda47951d8211
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2023-07-18 10:31:35 +0300

    refactor(18.07.23): add componentDidMount in Users component

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -6,10 +6,14 @@ import userPhoto from '../../../src/assets/images/user.png'
 class Users extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
     axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
       this.props.setUsers(response.data.items);
     })
   }
+  
   render() {
     return (
       <div>
