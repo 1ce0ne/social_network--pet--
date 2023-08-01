be5c5484566393da881116b1bc7d41327907b6bb
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2023-08-01 18:00:36 +0300

    feature(01.08.23): Added error incorrect email or password

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,6 +5,7 @@ import { required } from '../../utils/validators/validators';
 import { connect } from 'react-redux';
 import { login } from '../../redux/authReducer';
 import { Navigate } from 'react-router-dom';
+import styles from '../common/FormsControls/FormsControls.module.css';
 
 const LoginForm = (props) => {
   return (
@@ -30,6 +31,7 @@ const LoginForm = (props) => {
         <Field type={'checkbox'} name={'rememberMe'} component={Input} />
         remember me
       </div>
+      {props.error && <div className={styles.formSummaryError}>{props.error}</div>}
       <div>
         <button>Login</button>
       </div>
